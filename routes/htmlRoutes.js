e7599b71f9c05dc4e14106fce91444945d18464b
ceewizz <ceewizzhuhwin@gmail.com>
2023-12-20 20:44:46 -0800

    fixed htmlRoutes.js files to the right directory

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,11 +5,11 @@ module.exports = (app) => {
 
     // creating the routes and notes should return into a html file.
     app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, './routes/public/notes.html'));
+        res.sendFile(path.join(__dirname, './public/notes.html'));
     })
 
     // * return the index.html file
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './routes/public/index.html'));
+        res.sendFile(path.join(__dirname, './public/index.html'));
     })
 };
