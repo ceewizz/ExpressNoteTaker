671ca2f1b1fc1f606cc24c014da381355525b282
ceewizz <ceewizzhuhwin@gmail.com>
2023-12-21 00:12:27 -0800

    fixed more server issues from server.js

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ app.use(express.json());
 
 
 // package allow a unique path name to be created   
-var uniqid = require('uniqid');
+const uniqid = require('uniqid');
 
     // A new note is created, then add it to the db.json file which then 
     // returns to the user.
 
-    app.get('api/notes', (req, res) => {
+    app.get('/api/notes', (req, res) => {
         let db = fs.readFileSync('./db/db.json', (err, data) => {
-        if (error) throw error;
+        if (err) throw err;
         let dbData = JSON.parse(data);
         res.json(dbData);
  });
@@ -38,12 +38,11 @@ var uniqid = require('uniqid');
     // Create a new note
     const userNote = req.body 
     userNote.id = uniqid()
-      // The note create
-      id: uniqid(),
+      
 
   // Push created note over to db.json file
   db.push(userNote);
-  fs.writeFileSync('./db/db.json', JSON.stringify(db));
+  fs.writeFileSync('./db/db.json', JSON.stringify(db))
   res.json(db);
 }) 
 
@@ -53,10 +52,12 @@ var uniqid = require('uniqid');
         app.delete('/api/notes/:id', (req, res) => {
             // getting the notes from the db.json file
                // removing the name of the file
-            const deleteNotes  =  db.filter(item => item.id !== req.params.id);
+            const deleteNotes  =  db.filter((note) =>
+             note.id !== req.params.id)
 
 
             // Then write the new notes to the db.json file
+
             fs.writeFileSync('./db/db.json', JSON.stringify(deleteNotes));
             readFile.json(deleteNotes)
 
@@ -70,7 +71,7 @@ var uniqid = require('uniqid');
 
         
     app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'notes,html'))
+        res.sendFile(path.join(__dirname, 'public', 'notes.html'))
     })
 
     
